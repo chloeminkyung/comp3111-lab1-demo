398fff05d583abbdea69d11deacac243b9431578
Chloe Kim <chloeminkyung@gmail.com>
2018-09-06 12:01:23 +0800

    edit print statement

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -5,7 +5,7 @@ public class Library {
     public static void main(String arg[]) {
     	Library lib = new Library();
     	if (lib.someLibraryMethod())
-    		System.out.println("When ther is a fire, commit and push.");
+    		System.out.println("When ther is a fire, commit and push to your branch.");
     }
     
     public boolean someLibraryMethod() {
